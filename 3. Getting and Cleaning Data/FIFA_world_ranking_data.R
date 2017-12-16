06bbda8e556dade2f7fcb5c32de847003710f6e7
cmumulle <christian_a_mueller@me.com>
2017-12-16 17:04:56 +0100

    Updated FIFA file with html_table()

--- 3. Getting and Cleaning Data/FIFA_world_ranking_data.R
+++ 3. Getting and Cleaning Data/FIFA_world_ranking_data.R
@@ -2,13 +2,28 @@
 ## head(text)
 
 library("rvest", lib.loc="/Library/Frameworks/R.framework/Versions/3.4/Resources/library")
-library("dplyr", lib.loc="/Library/Frameworks/R.framework/Versions/3.4/Resources/library")
 
-page <- read_html("http://www.fifa.com/fifa-world-ranking/ranking-table/men/index.html")
-head(text)
+url <- "http://www.fifa.com/fifa-world-ranking/ranking-table/men/index.html"
 
-selector <- "#profile > div:nth-child(2) > table > tbody"
-text <- page %>% html_nodes(css = selector) %>% html_text()
+world_ranking <- url %>%
+        html() %>%
+        html_nodes(xpath='//*[@id="profile"]/div[2]/table') %>%
+        html_table()
 
-xpath <- '//*[@id="profile"]/div[2]/table/tbody'
-text <- page %>% html_nodes(xpath = xpath) %>% html_text()
+fifa_world_ranking <- world_ranking[[1]]
+
+head(fifa_world_ranking)
+
+## GRAVEYARD
+## library("dplyr", lib.loc="/Library/Frameworks/R.framework/Versions/3.4/Resources/library")
+## library("XML", lib.loc="/Library/Frameworks/R.framework/Versions/3.4/Resources/library")
+## page <- read_html("http://www.fifa.com/fifa-world-ranking/ranking-table/men/index.html")
+## head(text)
+## selector <- "#profile > div:nth-child(2) > table > tbody"
+## text <- page %>% html_nodes(css = selector) %>% html_text()
+## xpath <- '//*[@id="profile"]/div[2]/table/tbody'
+## text <- page %>% html_nodes(xpath = xpath) %>% html_text()
+## df <- html_table("http://www.fifa.com/fifa-world-ranking/ranking-table/men/index.html")
+## library("rvest")
+        
+        
